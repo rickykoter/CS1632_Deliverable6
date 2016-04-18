892f12ddef00aae3b8f8ddbb8584f6779403e9f8
Richard Kotermanski <rek58@pitt.edu>
2016-04-18 19:28:37 -0400

    Added comments for ChatClient

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,3 +1,7 @@
+/**
+ * ChatClient manages a Connection by receiving (listening) and sending messages under an alias.
+ * By Richard Kotermanski and Jon Povirk
+ */
 public class ChatClient implements Client{
     private Connection _connection;
     private String _alias;
@@ -8,7 +12,9 @@ public class ChatClient implements Client{
         _alias = "Anonymous";
         _crt = new ClientReceiveThread();
     }
-
+    
+    // Returns true and sets the connection to given Connection if it is open and not null; 
+    // otherwise returns false.
     public boolean connect(Connection connection) {
         if(connection != null && connection.isOpen()){
              _connection = connection;
@@ -18,7 +24,8 @@ public class ChatClient implements Client{
             return false;
         }
     }
-
+    
+    // Returns true if the current connection is not null and is open; returns false otherwise.
     public synchronized boolean isConnected() {
         if(_connection == null || !_connection.isOpen()){
             return false;
@@ -26,7 +33,9 @@ public class ChatClient implements Client{
             return true;
         }
     }
-
+    
+    // If the current connection is not null and open, then return true, disconnect the connection, 
+    // and set the current connection to null. If not connected, then return false.
     public synchronized boolean disconnect() {
         if(isConnected()){
             boolean res =_connection.disconnect();
@@ -36,7 +45,9 @@ public class ChatClient implements Client{
             return false;
         }
     }
-
+    
+    // Sends a message using the current connection. Returns true if the current Connection is 
+    // connected and returns true for its send function. Otherwise returns false.
     public boolean send(Message message) {
         if(isConnected()){
             return _connection.send(message);
@@ -45,10 +56,13 @@ public class ChatClient implements Client{
         }
     }
 
+    // Returns the alias of the client
     public String getAlias() {
         return _alias;
     }
-
+    
+    // If the desired alias "a" is beween 1 and 15 characters, then set the Clients alias to it and return true;
+    // otherwise, return false.
     public boolean setAlias(String a) {
         if(a.length() > 15 || a.length() < 1){
             return false;
@@ -58,6 +72,8 @@ public class ChatClient implements Client{
         }
     }
 
+    // Starts and returns a thread that is listening for messages for the current Connection while it is connected
+    // and without exception.
     public Thread beginReceiving() {
         _crt.start();
         return _crt;
