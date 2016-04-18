02b6d29522cf627a7de08531d676248107dc20e8
Richard Kotermanski <rek58@pitt.edu>
2016-04-18 18:36:39 -0400

    added commenting to ChatApp for functions

--- src/ChatApp.java
+++ src/ChatApp.java
@@ -9,7 +9,10 @@ import java.net.Socket;
 import java.nio.channels.ServerSocketChannel;
 import java.util.HashSet;
 import java.util.LinkedList;
-
+/**
+ * Main class and GUI. Allows for user interaction with client and server creation and messag sending.
+ * By Richard Kotermanski and Jon Povirk
+ */
 public class ChatApp extends JFrame {
     private JPanel mainPanel;
     private JTextField hostTextField;
@@ -30,7 +33,8 @@ public class ChatApp extends JFrame {
     private Client client;
     private SocketFactory socketFactory;
     private Server server;
-
+    
+    // Handles button actions and displaying result to the text output area in th GUI.
     public ChatApp(Client c, SocketFactory sf) {
         System.setOut(new PrintStream(new TextAreaOutputStream(chatTextArea)));
         client = c;
@@ -66,7 +70,9 @@ public class ChatApp extends JFrame {
             }
         });
     }
-
+    
+    // Attempt to create a Client and its Connection to a corresponding Server at the given hostName an portNumber, and
+    // set the client's alias if between 1 and 15 characters. Sets button enabled states for GUI.
     public String connectToServer(String hostName, String portNumber, String alias) {
         if (client == null) {
             client = new ChatClient();
@@ -117,7 +123,9 @@ public class ChatApp extends JFrame {
         client.beginReceiving();
         return "You have been successfully connected to " + hostName + " at port " + portNumber + ".";
     }
-
+    
+    // Attempts to disconnect from the server if client is already connected and returns success/failure message. 
+    // Sets button enabled states for GUI.
     public String disconnectFromServer() {
         if (client.isConnected() && client.disconnect()) {
             connectButton.setEnabled(true);
@@ -139,7 +147,9 @@ public class ChatApp extends JFrame {
             return "Error: You were unable to be disconnected!";
         }
     }
-
+    
+    // Attempts to send messages of length 1 or more to the currently connected Server if connected.
+    // Returns empty string if successful, and appropriate error message otherwise. Sets button enabled states for GUI.
     public String sendMessageToServer(String MessageText) {
         if (MessageText.length() <= 0) {
             return "Error: Please provide text to send!";
@@ -151,6 +161,8 @@ public class ChatApp extends JFrame {
         }
     }
 
+    // Attempts to create Server for the provided hostName and portNumber, and attempts to create client 
+    // for the server with the given alias. Returns a status message upon success/failure. Sets button enabled states for GUI.
     public String startServer(String hostName, String portNumber, String alias) {
         connectButton.setEnabled(true);
         disconnectButton.setEnabled(false);
